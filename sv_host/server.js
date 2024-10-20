518f64f9899f3a0c39889d73ed4f1e9dba320ce9
jinhyeok_choi <first2021@kaist.ac.kr>
2024-10-20 23:32:40 +0900

    v1.06

--- sv_host/server.js
+++ sv_host/server.js
@@ -54,7 +54,7 @@ const app = express();
 const server = http.createServer(app);
 const port = 4000;
 
-const version = 1.05;
+const version = 1.06;
 
 import { fileURLToPath } from 'url';
 
@@ -185,6 +185,7 @@ app.post('/set/motor', (req, res) => {
   }
 
   speed = 100-speed;
+  steer = 90-steer;
   
   //make value to the required format
   const steerstr = steer.toString();
