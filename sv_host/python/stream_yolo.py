a3878001763b506ab67f055260dbbfc42eafd9f3
jinhyeok_choi <first2021@kaist.ac.kr>
2024-11-15 18:45:14 +0900

    1.12

--- sv_host/python/stream_yolo.py
+++ sv_host/python/stream_yolo.py
@@ -16,8 +16,6 @@ elif architecture == 'x86_64':
     url = 'http://121.184.63.113:4000/center'
 
 compression_rate = 50  # Initial compression rate
-desired_fps = 10      # Set the desired frames per second
-frame_interval = 1 / desired_fps  # Calculate time interval between frames
 
 def center_crop(image):
     h, w = image.shape[:2]
@@ -37,30 +35,24 @@ def center_crop(image):
     
     return cropped_image
 
-last_frame_time = 0  # Track the last frame's timestamp
-
 while True:
-    current_time = time.time()
-    elapsed_time = current_time - last_frame_time
+    start_time = time.time()  # Track time before sending the image
 
-    if elapsed_time >= frame_interval:
-        last_frame_time = current_time
+    frame = jajucha2.camera.get_image()
 
-        frame = jajucha2.camera.get_image()
+    # Center crop the image to make it square
+    cropped_frame = center_crop(frame)
 
-        # Center crop the image to make it square
-        cropped_frame = center_crop(frame)
+    # Resize the cropped image to 640x640
+    resized_frame = cv2.resize(cropped_frame, (640, 640))
+    
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), compression_rate]
+    _, buffer = cv2.imencode('.jpg', resized_frame, encode_param)
 
-        # Resize the cropped image to 640x640
-        resized_frame = cv2.resize(cropped_frame, (640, 640))
-        
-        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), compression_rate]
-        _, buffer = cv2.imencode('.jpg', resized_frame, encode_param)
+    jpg_as_text = base64.b64encode(buffer).decode('utf-8')
 
-        jpg_as_text = base64.b64encode(buffer).decode('utf-8')
+    data = {'image': jpg_as_text}
+    response = requests.post(url, json=data)
 
-        data = {'image': jpg_as_text}
-        response = requests.post(url, json=data)
-
-        if response.status_code != 200:
-            print('Failed to send data')
+    if response.status_code != 200:
+        print('Failed to send data')
